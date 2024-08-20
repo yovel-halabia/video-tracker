91a6d33ea9cfacf9ed31e04fbec4c840eb4873fc
yovel-halabia <yovelh247@gmail.com>
2024-08-20 12:58:22 +0300

    frontend build with prev changes

--- backend/wwwroot/dist/assets/utils/HttpOptions.ts
+++ backend/wwwroot/dist/assets/utils/HttpOptions.ts
@@ -1,11 +1,11 @@
 import {HttpHeaders} from "@angular/common/http";
 import Cookie from "./Cookie";
 
-const HttpOptions = {
-	headers: new HttpHeaders({
-		"Content-Type": "application/json",
-		Authorization: `Bearer ${Cookie.get("accessToken")}`,
-	}),
-};
-
-export default HttpOptions;
+export default function () {
+	return {
+		headers: new HttpHeaders({
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${Cookie.get("accessToken")}`,
+		}),
+	};
+}
